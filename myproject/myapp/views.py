4b0ec64e26433c44119432978bc698301a27cb07
Abdullah Tareq <abdullah.mohammad02@cba.ku.edu.kw>
2021-12-03 14:20:17 +0300

    hw2

---
+++ myproject/myapp/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def test_view(request):
+    print("request: ", request.GET["pwd"])
+   
+    
+
+
+    c = {}
+    c["username"] = request.GET.get('username')
+    c["age"] = 40
+    c["majors"] = [
+        "MIS",
+        "ORM",
+        "FINANCE",
+        "ACCOUNTING",
+        "ECONOMIC",
+        "PUBLIC ADMIN",
+    ]
+
+    return render(request, "template2.html", c )
+
